aabb7112af535916661de93d39c6285c44e942a7
ashiarora2804 <52677950+ashiarora2804@users.noreply.github.com>
2019-07-23 13:23:21 -0400

    Add files via upload

---
+++ github/assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { Books } from '../../app/bookInfo';
+
+export const BOOK: Books[]=
+[
+    {name:"Hunger Games", genre: "Science Fiction", year:2008 , author: "Suzanne Collins", pic : 'assets/images/hungergames.jpg'},
+    {name:"Harry Potter", genre: "Drama", year:2001 , author: "JK Rowling", pic : 'assets/images/harrypotter.jpg'},
+    {name:"Pride and Prejudice", genre: "Fiction", year:1813 , author: "Jane Auston", pic : 'assets/images/prideandprejudice.jpg'},
+    {name:"Gone With The Wind", genre: "Historical Drama", year:1940 , author: "Margaret Mitchell", pic : 'assets/images/gonewiththewind.jpg'}
+]
